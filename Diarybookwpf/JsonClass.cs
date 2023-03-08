52cee6f7d9d4d91722713257453bbad06b52fb69
igorvsx <igor.19840703@gmail.com>
2023-03-08 20:58:24 +0300

    a

--- Diarybookwpf/JsonClass.cs
+++ Diarybookwpf/JsonClass.cs
@@ -8,46 +8,20 @@ using System.IO;
 
 namespace Diarybookwpf
 {
-    public class JsonClass<T>
+    internal class JsonClass<T>
     {
-        //public void Serialize(string fileName, T data)
-        //{
-        //    var json = JsonConvert.SerializeObject(data);
-        //    File.WriteAllText(fileName, json);
-        //}
-
-        //public T Deserialize(string fileName)
-        //{
-        //    var json = File.ReadAllText(fileName);
-        //    return JsonConvert.DeserializeObject<T>(json);
-        //}
-        //public static void Serialize<T>(string filePath, T obj)
-        //{
-        //    JsonSerializerSettings settings = new JsonSerializerSettings
-        //    {
-        //        TypeNameHandling = TypeNameHandling.All
-        //    };
-        //    string json = JsonConvert.SerializeObject(obj, settings);
-        //    File.WriteAllText(filePath, json);
-        //}
-
-        //public static T Deserialize<T>(string filePath)
-        //{
-        //    JsonSerializerSettings settings = new JsonSerializerSettings
-        //    {
-        //        TypeNameHandling = TypeNameHandling.All
-        //    };
-        //    string json = File.ReadAllText(filePath);
-        //    return JsonConvert.DeserializeObject<T>(json, settings);
-        //}
-        //public static string Serialize<T>(T obj)
-        //{
-        //    return JsonConvert.SerializeObject(obj);
-        //}
-
-        //public static T Deserialize<T>(string json)
-        //{
-        //    return JsonConvert.DeserializeObject<T>(json);
-        //}
+        public static void Serialize<T>(T notes, string fileName)
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string json = JsonConvert.SerializeObject(notes);
+            File.WriteAllText(desktop + "\\" + fileName, json);
+        }
+        public static T Deserialize<T>(string fileName)
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string json = File.ReadAllText(desktop + "\\" + fileName);
+            T notes = JsonConvert.DeserializeObject<T>(json);
+            return notes;
+        }
     }
 }
--- Diarybookwpf/MainWindow.xaml.cs
+++ Diarybookwpf/MainWindow.xaml.cs
@@ -7,29 +7,21 @@ using System.IO;
 using System.Globalization;
 using System.Linq;
 
-//{d:SampleData ItemCount=5}
 namespace Diarybookwpf
 {
     public partial class MainWindow : Window
     {
-
         private List<Note> notes = new List<Note>();
+        
         private string notesFilePath = "C:\\Users\\Igor\\Desktop\\notes.json";
         public MainWindow()
         {
             InitializeComponent();
-            datePicker.Text = DateTime.Today.ToString();
-            datePicker.DisplayDate = DateTime.Today;
+            datePicker.SelectedDate = DateTime.Today;
             if (File.Exists(notesFilePath))
             {
                 notes.Clear();
-                string json = File.ReadAllText(notesFilePath);
-                notes = JsonConvert.DeserializeObject<List<Note>>(json);
-                //foreach (Note note in notes)
-                //{
-                //    MessageBox.Show(note.ToString());
-                //}
-                //datePicker.SelectedDate = selectedDate;
+                notes = JsonClass<List<Note>>.Deserialize<List<Note>>("notes.json");
 
                 ShowNotesForSelectedDate();
             }
@@ -40,8 +32,7 @@ namespace Diarybookwpf
         }
         private void SaveNotesToFile()
         {
-            string json = JsonConvert.SerializeObject(notes);
-            File.WriteAllText(notesFilePath, json);
+            JsonClass<List<Note>>.Serialize(notes, "notes.json");
         }
 
         private void ShowNotesForSelectedDate()
@@ -56,30 +47,14 @@ namespace Diarybookwpf
                 {
                     if (selectedDate == note.Date)
                     {
-
                         notesList.Items.Add(note.Title);
-
                     }
                 }
             }
-
-
         }
         private void datePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             notesList.SelectionChanged -= notesList_SelectionChanged;
-            //noteTitle.Text = "";
-            //noteDescription.Text = "";
-            //notesList.Items.Clear();
-            //DateTime selectedDate = datePicker.SelectedDate.GetValueOrDefault();
-            //foreach (Note note in notes)
-            //{
-            //    if (/*Convert.ToDateTime(datePicker.SelectedDate).Day == note.Date.Day*/selectedDate == note.Date)
-            //    {
-            //        notesList.Items.Add(note.Title);
-            //    }
-            //}
-            //selectedDate = datePicker.SelectedDate ?? DateTime.Today;
             ShowNotesForSelectedDate();
             notesList.SelectionChanged += notesList_SelectionChanged;
         }
@@ -102,8 +77,6 @@ namespace Diarybookwpf
         }
         private void AddNote()
         {
-            //List<Note> new_notes = new List<Note>();
-            //List<Note> old_notes = new List<Note>();
             DateTime selectedDate = datePicker.SelectedDate.GetValueOrDefault();
             if (noteTitle.Text != "" & noteDescription.Text != "")
             {
@@ -116,33 +89,8 @@ namespace Diarybookwpf
                     notes = new List<Note>();
                 }
                 notes.Add(note);
-                //{
-                //    Title = noteTitle.Text,
-                //    Description = noteDescription.Text,
-                //    Date = selectedDate
-                //};
-                //foreach (string item in sourceList)
-                //{
-                //    if (targetList == null)
-                //    {
-                //        targetList = new List<string>();
-                //    }
-
-                //    targetList.Add(item);
-                //}
-                //if (notes == null)
-                //{
-                //    new_notes.Add(note);
-                //    notes.AddRange(new_notes);
-                //}
-                //else
-                //{
-                //    notes.Add(note);
-                //}
-
                 SaveNotesToFile();
 
-                // Show notes for selected date
                 ShowNotesForSelectedDate();
             }
             else
@@ -152,7 +100,6 @@ namespace Diarybookwpf
         }
         private void ShowNoteDetails()
         {
-            //ShowNotesForSelectedDate();
             Note note = notes[notesList.SelectedIndex];
             noteTitle.Text = note.Title;
             noteDescription.Text = note.Description;
@@ -164,86 +111,11 @@ namespace Diarybookwpf
             {
                 ShowNoteDetails();
             }
-            //foreach (Note note in notes)
-            //{
-            //    if (notesList.SelectedItem.ToString() == note.Title)
-            //    {
-            //        //Note note = (Note)notesList.SelectedItem;
-
-            //        noteTitle.Text = note.Title;
-            //        noteDescription.Text = note.Description;
-            //    }
-            //}
-
-            //if (notesList.SelectedItem != null)
-            //{
-            //    Note note = (Note)notesList.SelectedItem;
-
-            //    noteTitle.Text = note.Title;
-            //    noteDescription.Text = note.Description;
-            //}
-            //else
-            //{
-            //    // Clear note details
-            //    noteTitle.Text = "";
-            //    noteDescription.Text = "";
-            //}
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            //var selectedNoteTitle = (string)notesList.SelectedValue;
-            //var selectedNote = notes.FirstOrDefault(n => n.Title == selectedNoteTitle);
-
-            //if (selectedNote == null)
-            //{
-            //    MessageBox.Show("Выберите заметку для удаления", "Ошибка");
-            //    return;
-            //}
-
-            //notes.Remove(selectedNote);
-            //SaveNotesToFile();
-            //ShowNotesForSelectedDate();
-
             DeleteNote();
-
-            //if (notesList.SelectedItem != null)
-            //{
-            //    var selectedNote = (Note)notesList.SelectedItem;
-            //    notes.Remove(selectedNote);
-            //    //DateTime selectedDate = datePicker.SelectedDate.GetValueOrDefault();
-
-            //    //foreach (Note note in notes)
-            //    //{
-            //    //    if (notesList.SelectedItem.ToString() == note.Title)
-            //    //    {
-            //    //        notesList.Items.Remove(note);
-            //    //        notes.Remove(note);
-            //    //        SaveNotesToFile();
-            //    //        notesList.Items.Clear();
-            //    //    }
-            //    //}
-            //    SaveNotesToFile();
-            //    ShowNotesForSelectedDate();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Выберите дату, у которой хотите удалить заметку");
-            //}
-
-            //if (notesList.SelectedItem != null)
-            //{
-            //    // Delete selected note
-            //    Note note = (Note)notesList.SelectedItem;
-            //    notes.Remove(note);
-
-            //    // Save notes to file
-            //    SaveNotesToFile();
-
-            //    // Show notes for selected date
-            //    ShowNotesForSelectedDate();
-            //}
-
         }
 
         private void CreateButton_Click(object sender, RoutedEventArgs e)
@@ -254,25 +126,6 @@ namespace Diarybookwpf
         private void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
             UpdateNote();
-            //if (notesList.SelectedItem != null)
-            //{
-            //    Note note = notes[notesList.SelectedIndex];
-            //    note.Title = noteTitle.Text;
-            //    note.Description = noteDescription.Text;
-            //    //ShowNotes();
-            //    //SaveNotes();
-            //    // Update selected note
-            //    //Note note = (Note)notesList.SelectedItem;
-            //    //note.Title = noteTitle.Text;
-            //    //note.Description = noteDescription.Text;
-            //    ShowNotesForSelectedDate();
-            //    SaveNotesToFile();
-            //    // Save notes to file
-            //    //
-
-            //    //// Show notes for selected date
-            //    //
-            //}
         }
     }
 }
